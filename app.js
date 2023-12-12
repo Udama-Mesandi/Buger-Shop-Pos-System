0c3f9cf5e98e4c07c428ec98e767d55a6433e724
UDAMA_MESANDI <wumpeiris@gmail.com>
2023-12-12 19:39:35 +0530

    sample commit1

---
+++ app.js
@@ -0,0 +1,47 @@
+// function Adminlogin(){
+//     var x=document.getElementById("user");
+//     var y=document.getElementById("Admin");
+//     if(y.style.display === "none"){
+//         x.style.display="none";
+//         y.style.display="block";
+//     }else{
+//         x.style.display="block";
+//         y.style.display="none";
+//     }
+// }
+
+// function Chashierlogin(){
+//     var x=document.getElementById("user");
+//     var y=document.getElementById("Chashier");
+//     if(y.style.display === "none"){
+//         x.style.display="none";
+//         y.style.display="block";
+//     }else{
+//         x.style.display="block";
+//         y.style.display="none";
+//     }
+// }
+
+// function backButton(){
+//     var x=document.getElementById("user");
+//     var y=document.getElementById("Admin");
+//     var z=document.getElementById("Chashier");
+
+//     if(y.style.display ==="block"){
+       
+//         x.style.display="block";
+//         y.style.dispaly="none";
+        
+
+//     }else if(z.style.display ==="block") {
+        
+//         x.style.display="block";
+//         z.style.dispaly="none";
+//     }
+
+//     // }else{
+//     //     x.style.display="block";
+//     // }
+
+// }
+
